bdfbd2ce4ef60f6b4e9d9b235aa424840acf4394
AlexDjangoX <alexmonk17@gmail.com>
2023-01-11 12:06:53 +0000

    added event-detail folder with components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,33 @@
-import React from 'react';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data.js';
+import EventSummary from '../../component/event-detail/event-summary.js';
+import EventLogistics from '../../component/event-detail/event-logistics.js';
+import EventContent from '../../component/event-detail/event-content.js';
 
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <h4>No event found</h4>;
+  }
+
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
